af109af9ed9446f88e0ca1144ed234a601e62acc
Bernardo Garcia <botibagl@gmail.com>
2017-05-06 14:18:50 -0500

    settings

--- ihost/settings/base.py
+++ ihost/settings/base.py
@@ -172,6 +172,11 @@ EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 AUTH_USER_MODEL = "accounts.User"
 
+#Amazon S3 Storage
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =  get_env_variable('AWS_SECRET_ACCESS_KEY')
+
 # Default settings
 BOOTSTRAP3 = {
 

b5f54dbe1e9d69a668ea2ab38599e31158738355
Bernardo Garcia <botibagl@gmail.com>
2017-08-10 11:01:04 -0500

    fixing the articles image containers

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Article, Comment
 
 
-admin.site.register(Article)
-admin.site.register(Comment)
+# admin.site.register(Article)
+# admin.site.register(Comment)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+     list_display = ('id', 'author', 'title', 'created_date',
+        'published_date')
